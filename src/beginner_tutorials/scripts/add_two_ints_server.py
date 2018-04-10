bfae113869f656432349b172808f41fb30a7b5b1
jds110 <jds110@live.missouristate.edu>
2018-04-10 16:38:01 -0500

    Modified 'add_two_ints_server.py'

--- src/beginner_tutorials/scripts/add_two_ints_server.py
+++ src/beginner_tutorials/scripts/add_two_ints_server.py
@@ -5,13 +5,13 @@ import rospy
 
 def handle_add_two_ints(req):
     print "Returning [%s + %s = %s]"%(req.a, req.b, (req.a + req.b))
-    return ADdTwoIntsResponse(req.a + req.b)
+    return AddTwoIntsResponse(req.a + req.b)
 
 def add_two_ints_server():
-    respy.init_node('add_two_ints_server')
-    s = respy.Service('add_two_ints', AddTwoInts, handle_add_two_ints)
+    rospy.init_node('add_two_ints_server')
+    s = rospy.Service('add_two_ints', AddTwoInts, handle_add_two_ints)
     print "Ready to add two ints."
-    respy.spin()
+    rospy.spin()
 
 if __name__ == "__main__":
     add_two_ints_server()
